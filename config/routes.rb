6415a621dae2283715c56f1cfcb2e2c8e6991275
Brett Pfefferman <brettpfefferman@yahoo.com>
2017-07-15 12:57:29 -0700

    intial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: 'tweets#index'
+
+get 'tweets', to: 'tweets#index' 
+
+get 'tweets/:id', to: 'tweets#show', as: :tweet
+
+
+
+
+end
